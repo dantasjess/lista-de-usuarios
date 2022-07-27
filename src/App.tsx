40d475871ca6260b0c55c557e18e0f74aa9d344f
Jessica <jessica.dantas.rocha@gmail.com>
2022-07-27 17:57:39 -0300

    aula 27/07

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import CommentIcon from '@mui/icons-material/Comment';
+import IconButton from '@mui/material/IconButton';
+
+const Lista = (props: any) => {
+  return (
+    <List sx={{ width: '100%', maxWidth: 720, bgcolor: 'background.paper'}}>
+      {props.usuarios.map((value:any) => (
+        <ListItem>
+          <ListItemText primary={`Nome: ${value.name}`} /> <br/>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
+
+const App = (props: any) => {
+	const [users, setUsers] = useState([
+		{ id: 1, name: "Minora" },
+		{ id: 2, name: "Ataide" },
+	]);
+  const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/users/")
+			.then((response) => response.json())
+			.then((json) => {setUsers(json); setLoading(false)});
+	});
+	return (
+		<div className="App">
+			<h1>Lista de usuários</h1>
+
+      <Lista usuarios={users}></Lista>
+
+			<div className="card">
+        {loading ? <h2>Carregando...</h2> : null}
+				{/* <ul>
+					{users.map((user) => (<li key={user.id}>{user.name}</li>
+					))}
+				</ul> */}
+			</div>
+		</div>
+	);
+};
+
+export default App;
